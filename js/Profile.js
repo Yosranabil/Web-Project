3d24d90302595bf4ece1d64ba247ed9b1ce6ed88
yassmen <smsmaashraf40@gmail.com>
2022-08-26 03:26:46 +0200

    display info and store it in localstorage

--- js/Profile.js
+++ js/Profile.js
@@ -1,4 +1,5 @@
-//declaring
+//photo...declaring
+
 const imgdiv = document.querySelector(".img-icon");
 const img = document.querySelector("#photo");
 const file = document.querySelector("#file");
@@ -35,4 +36,65 @@ imgdiv.addEventListener('mouseleave',function()
     }
 
   });
- 
+  //user_data
+
+  accounts = JSON.parse(localStorage.getItem("accounts")) ;
+  function load_user(){
+    var currentUser = localStorage.getItem("currentUser");
+    accounts.indexOf(currentUser);
+  if(typeof currentUser === undefined || currentUser === null){
+    currentUser = JSON.stringify({uname:"",pass:"",mail:"",phone:""});
+    localStorage.setItem("currentUser",currentUser);
+  }
+    
+
+    return JSON.parse(currentUser);
+  }
+
+  //reload the page
+  function save_user(uname , pass, mail, phone){
+    currentUser = JSON.stringify({uname:uname,pass:pass,mail:mail,phone:phone,type:"user"});
+    localStorage.setItem("currentUser",currentUser);
+      return currentUser;
+  }
+  
+  
+  const submitbutton=document.getElementById("submit");
+   
+  submitbutton.addEventListener("click",function(){
+    save_user(
+    document.getElementById('uname').value,
+    document.getElementById('pass').value,
+    document.getElementById('mail').value,
+    document.getElementById('phone').value,
+    
+    );
+    currentUser = load_user();
+    document.getElementById('uname').value = currentUser.uname;
+    document.getElementById('pass').value = currentUser.pass;
+    document.getElementById('mail').value = currentUser.mail;
+    document.getElementById('phone').value = currentUser.phone;
+    
+  });
+  
+  currentUser= load_user();
+  
+  //id of page
+  document.getElementById('uname').value = currentUser.uname;
+  document.getElementById('pass').value = currentUser.pass;
+  document.getElementById('mail').value = currentUser.mail;
+   document.getElementById('phone').value = currentUser.phone;
+  
+   //currentUser=update_user;
+   let newaccount=
+   {
+   uname : uname.value,
+   pass : pass.value,
+   mail : mail.value,
+   phone : phone.value,
+   type: "user"
+   }
+
+accounts.push(newaccount);
+localStorage.setItem("accounts",JSON.stringify(accounts));
+  
